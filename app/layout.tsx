2b8b04eb983b6c1aceaa6c4a8229df1b106eb872
diakon1551-creator <diakon1551@gmail.com>
2026-07-15 10:21:07 +0300

    Add Lakonia favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "LAKONIA",
   description: "Місце, де слова мають вагу.",
   icons: {
